c0a2c232ad95aad39ca68159f3babb14f599b587
Ender Onryo <89392609+EnderOnryo@users.noreply.github.com>
2021-09-13 11:24:21 -0500

    Initial commit

---
+++ Convenient PoP/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Modding;
+using UnityEngine;
+
+namespace Convenient_PoP
+{
+    public class Convenient_PoP : Mod
+    {
+        public Convenient_PoP() : base("Convenient PoP") { }
+        public override string GetVersion() => "1.0";
+
+
+        public override void Initialize()
+        {
+            ModHooks.SlashHitHook += SlashHitHandler;
+        }
+
+        
+        /* This causes the player's soul to be filled when the player hits a PoP Soul Totem or a Wingsmould with their Nail.
+         * This does not work with any Narts.
+         */
+        public void SlashHitHandler(Collider2D otherCollider, GameObject slash)
+        {
+            if (otherCollider.name.StartsWith("White Palace Fly") || otherCollider.name.StartsWith("Soul Totem white_Infinte"))
+            {
+                HeroController.instance.AddMPCharge(PlayerData.instance.MPReserveMax);
+            }
+        }
+    }
+}
+
+/*To do
+ * Make Narts work
+ * Make Soul Orbs not spawn
+ * Add a canFocus check and if yes heal the player to full
+ * Make the mod appear in the Mod Menu so it can be disabled
+ */
